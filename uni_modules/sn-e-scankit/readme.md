8cb35764adcd80d7aa717f908eab2f7354f2df82
SingmyWP <singmywp@gmail.com>
2024-09-27 22:59:28 +0800

    修复了已知问题
    因疏忽未将 Demo 项目的 sn-label-checkbox.uvue、sn-label-radio.uvue、sn-picker.uvue、sn-picker-view.uvue、sn-sidebar.uvue、scankit.uvue 以及 static 目录的部分图片资源、sn-e-scankit 插件目录等文件同步至 git，现已推送

---
+++ uni_modules/sn-e-scankit/readme.md
@@ -0,0 +1,12 @@
+# 文档
+此插件是扩展插件，基于华为统一扫码服务（Scan Kit）封装。Scan Kit不仅在常规扫码场景有着优异表现，还对复杂扫码场景（如反光、暗光、污损、小码、大角度等）做了特定识别优化，如可以实现远距离码或小型码地自动缩放和检测，扫码成功率有大幅提升。
+
+此插件提供最基础的 Default View 模式，支持相机扫码、导入图片扫码，不能自定义扫码界面，自由度较低。但使用方法相较于基于Google MLKit 的 sn-scan / sn-e-scan-provider 组件更为简单，且识别速度、准确率更是遥遥领先。
+
+
+详细文档请看：[ScanKit 华为扫码](https://sinleui.pages.dev/components/sn-e-scankit)
+
+# SinleUI
+一个简洁、轻巧的现代移动应用UI框架
+
+[文档地址](https://singmywp.github.io/)
---
+++ uni_modules/sn-e-scankit/utssdk/app-android/scankit.kt
@@ -0,0 +1,95 @@
+// 混编示例中的包名
+package uts.sdk.modules.snEScankit
+
+import android.app.ActivityManager
+import android.content.Context.ACTIVITY_SERVICE
+// UTS内置对象的包名引用
+import io.dcloud.uts.UTSAndroid
+import io.dcloud.uts.UTSArray
+import io.dcloud.uts.console
+import com.huawei.hms.ml.scan.*
+import com.huawei.hms.hmsscankit.ScanUtil
+import android.content.Intent
+import android.os.Parcelable
+import android.widget.Toast
+
+val REQUEST_CODE_SCAN_ONE = 334;
+
+fun getResultType(code: Int): String {
+    val codeDescriptions = mapOf(
+        1001 to "articleNumber",
+        1009 to "contactDetail",
+        1010 to "driverInfo",
+        1002 to "emailContent",
+        1008 to "eventInfo",
+        1012 to "isbnNumber",
+        1011 to "coordinate",
+        1004 to "text",
+        1005 to "sms",
+        1003 to "telephone",
+        1006 to "url",
+        1007 to "wifiInfo"
+    )
+    return codeDescriptions[code] ?: "text"
+}
+
+
+fun getScanType(type: String): Int {
+    return when (type.uppercase()) {
+        "CODE_128" -> HmsScan.CODE128_SCAN_TYPE
+        "CODE_39" -> HmsScan.CODE39_SCAN_TYPE
+        "CODE_93" -> HmsScan.CODE93_SCAN_TYPE
+        "CODABAR" -> HmsScan.CODABAR_SCAN_TYPE
+        "EAN_13" -> HmsScan.EAN13_SCAN_TYPE
+        "EAN_8" -> HmsScan.EAN8_SCAN_TYPE
+        "ITF_14" -> HmsScan.ITF14_SCAN_TYPE
+        "UPC_A" -> HmsScan.UPCCODE_A_SCAN_TYPE
+        "UPC_E" -> HmsScan.UPCCODE_E_SCAN_TYPE
+        "QRCODE" -> HmsScan.QRCODE_SCAN_TYPE
+        "PDF_417" -> HmsScan.PDF417_SCAN_TYPE
+        "AZTEC" -> HmsScan.AZTEC_SCAN_TYPE
+        "DATAMATRIX" -> HmsScan.DATAMATRIX_SCAN_TYPE
+        "MULTI_FUNCTIONAL" -> HmsScan.MULTI_FUNCTIONAL_SCAN_TYPE
+        else -> HmsScan.QRCODE_SCAN_TYPE
+    }
+}
+
+fun getScanTypesArray(types: Array<String>): Array<Int> {
+    return types.map { getScanType(it) }.distinct().filterNotNull().toTypedArray()
+}
+
+fun startScan(
+	scanTypes: Array<String>,
+	titleType: Int = 0,
+	callback: (UTSArray<Any>) -> Unit
+) {
+	val context = UTSAndroid.getUniActivity()
+	val scanTypeArray = getScanTypesArray(scanTypes).toIntArray()
+	var options: HmsScanAnalyzerOptions;
+	options = HmsScanAnalyzerOptions.Creator()
+	.setHmsScanTypes(scanTypeArray[0],*scanTypeArray.drop(1).toIntArray())
+	.setViewType(titleType)
+	.setErrorCheck(true)
+	.create()
+	
+	ScanUtil.startScan(context!!, REQUEST_CODE_SCAN_ONE, options)
+	
+	UTSAndroid.onAppActivityResult { requestCode: Int, _, data: Intent? ->
+	    if (data != null && requestCode == REQUEST_CODE_SCAN_ONE) {
+			val errorCode: Int = data.getIntExtra(ScanUtil.RESULT_CODE, ScanUtil.SUCCESS)
+			if (errorCode == ScanUtil.SUCCESS) {
+				val obj = data.getParcelableExtra(ScanUtil.RESULT) as Parcelable?
+				if (obj != null) {
+					val res = obj as HmsScan
+					callback(
+						UTSArray(getResultType(res.getScanTypeForm()),res.getOriginalValue())
+					)
+			    }
+			}
+			if (errorCode == ScanUtil.ERROR_NO_READ_PERMISSION) {
+				val toast = Toast.makeText(UTSAndroid.getAppContext(), "请授权相册权限重试", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		}
+	}
+}
